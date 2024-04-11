9cdb7c0fb2d31044bfaee0faa47e4619d84f95fb
Rohan Joseph Mathew <56034964+ron-matt163@users.noreply.github.com>
2024-04-11 16:41:18 -0400

    Set up a logger

---
+++ src/logger.py
@@ -0,0 +1,9 @@
+import logging
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+file_handler = logging.FileHandler('../exploration_values.log')
+file_handler.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+file_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
