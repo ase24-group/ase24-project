3280d0cdd420b2eaa8e66f4dad48cd17cde795eb
Joshua Joseph <joshjoseph97@gmail.com>
2024-04-20 16:13:23 -0400

    :pencil2: add `b2_stats`

--- src/main.py
+++ src/main.py
@@ -25,7 +25,7 @@ def generate_makefile(csv_path):
 
     csv_budget = math.ceil(math.sqrt(len(data.rows)))
     rand_budget = int(0.9 * len(data.rows))
-    treatments = ["progressive", "SimAnnealing", "bonr", "rand", "ExpProgressive"]
+    treatments = ["progressive", "SimAnnealing", "bonr", "b2", "rand", "ExpProgressive"]
 
     id = f"base"
     all_targets.append(id)
