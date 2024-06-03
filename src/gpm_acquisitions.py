4e05abb8398331a20c6f6f3917304df528155b16
Rohan Joseph Mathew <56034964+ron-matt163@users.noreply.github.com>
2024-06-03 19:11:44 -0400

    upper confidence bound acqn function implementation

--- src/gpm_acquisitions.py
+++ src/gpm_acquisitions.py
@@ -1,7 +1,17 @@
 from utils import get_cumulative_density
+import math
 
 def PI_score(mean, std, best_d2h):
-    exploit_exploit_tradeoff = 0.01 # The paper says that this has to be manually chosen.
-    score = get_cumulative_density((mean - best_d2h - exploit_exploit_tradeoff)/std)
+    exploit_explore_tradeoff = 0.01 # The paper says that this has to be manually chosen.
+    score = get_cumulative_density((mean - best_d2h - exploit_explore_tradeoff)/std)
     return score
 
+# TODO: check the results for both input dim and overall dim
+def UCB_score(mean, std, lite_size, dim):
+    delta = 0.1
+    v = 1
+    gamma = (2 * math.log((lite_size**(dim/2 + 2) * math.pi**2)/3*delta))
+    # revisit the std coeff
+    std_coeff = (v*gamma)**0.5
+
+    return mean + std_coeff*std
