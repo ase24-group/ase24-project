83a8aeaf530babbeb30b159e9ac0601be6896aba
Rohan Joseph Mathew <56034964+ron-matt163@users.noreply.github.com>
2024-06-18 11:54:14 -0400

    Srinivas 2010 UCB score formula

--- src/gpm_acquisitions.py
+++ src/gpm_acquisitions.py
@@ -1,12 +1,13 @@
 from utils import get_cumulative_density, get_probability_density
 import math
+import sys
 
 
 def PI_score(mean, std, best_d2h):
     exploit_explore_tradeoff = (
         0.01  # The paper says that this has to be manually chosen.
     )
-    m = (mean - best_d2h - exploit_explore_tradeoff) / std
+    m = (mean - best_d2h - exploit_explore_tradeoff) / (std + sys.float_info.min)
     score = get_cumulative_density(m, mean, std)
     return score
 
@@ -14,7 +15,7 @@ def PI_score(mean, std, best_d2h):
 # Expected improvement score
 def EI_score(mean, std, best_d2h):
     exploit_explore_tradeoff = 0.01  # As recommended by Hoffman et al 2011
-    m = (mean - best_d2h - exploit_explore_tradeoff) / std
+    m = (mean - best_d2h - exploit_explore_tradeoff) / (std + sys.float_info.min)
     cum_density_coeff = mean - best_d2h - exploit_explore_tradeoff
     score = (cum_density_coeff * get_cumulative_density(m, mean, std)) + (
         std * get_probability_density(m, mean, std)
@@ -24,10 +25,12 @@ def EI_score(mean, std, best_d2h):
 
 
 # TODO: check the results for both input dim and overall dim
-def UCB_score(mean, std, lite_size, dim):
+def UCB_score(mean, std, lite_size, dark_size, dim):
     delta = 0.1
     v = 1
-    gamma = 2 * math.log((lite_size ** (dim / 2 + 2) * math.pi**2) / 3 * delta)
+    D = lite_size + dark_size
+    # gamma = 2 * math.log((lite_size ** (dim / 2 + 2) * math.pi**2) / 3 * delta)
+    gamma = 2 * math.log((D * lite_size**2 * math.pi**2) / 6 * delta)
     # revisit the std coeff
     std_coeff = (v * gamma) ** 0.5
 
