751a1b277e6c381de31b83565df4f36ddf072866
Joshua Joseph <joshjoseph97@gmail.com>
2024-07-01 18:44:53 -0400

    :pencil2: fix `progress.sh` for updated treatments and budgets

--- src/progress.sh
+++ src/progress.sh
@@ -1,1 +1,1 @@
-find ../results/stats/cat_a_b -mindepth 1 -type d | xargs -I{} sh -c 'echo $((37-$(ls {} | wc -l))) {}' | grep -v '^0' | sort -nr
+find ../results/stats/cat_a_b -mindepth 1 -type d | xargs -I{} sh -c 'echo $((111-$(ls {} | wc -l))) {}' | grep -v '^0' | sort -nr
